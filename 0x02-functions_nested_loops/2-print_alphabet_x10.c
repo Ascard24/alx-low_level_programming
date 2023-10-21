d0233849414faa3b7e55ff24926c8743c7c283da
Ascard24 <kennethanietienyong@gmail.com>
2023-10-21 13:20:07 -0700

    ahakj

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - prints the string _putchar;
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int j = 0;
+
+	while (j < 10)
+	{
+		int i = 97;
+
+		while (i < 123)
+		{
+			_putchar(i);
+			i++;
+		}
+		_putchar('\n');
+		j++;
+	}
+}
