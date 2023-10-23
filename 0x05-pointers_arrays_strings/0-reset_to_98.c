885a2360485b7d7498501ec6353591bc6463850b
Ascard24 <kennethanietienyong@gmail.com>
2023-10-23 14:05:04 -0700

    kajsian

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * reset_to_98 - Reset the parameter to 98
+ * @n: A pointer to the value set as 98
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
